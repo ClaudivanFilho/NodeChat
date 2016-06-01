5ca4f049569fe56858320c9dcc685d319cc6601b
claudivanfilho <j.claudivan.filho@gmail.com>
2016-06-01 18:06:16 -0300

    change facebook callback

--- config/face-auth.js
+++ config/face-auth.js
@@ -5,7 +5,7 @@ var User = require('./../models/user.model.js');
 passport.use(new FacebookStrategy({
     clientID: "290046521330776",
     clientSecret: "d324100182cb0c1bc4d41595e45d5f02",
-    callbackURL: "http://nodebase.herokuapp.com/auth/facebook/callback",
+    callbackURL: "http://lastchat.herokuapp.com/auth/facebook/callback",
     profileFields: ['emails', 'displayName', 'gender', 'name', 'picture']
   },
   function(accessToken, refreshToken, profile, done) {
